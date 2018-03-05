bc5d1f7a88132817caa56212496169da51e6aa5f
Victor Lee <myleevictor@gmail.com>
2018-03-05 14:11:44 +0800

    added src, html and css files

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-tasks = ["buy milk", "eat dinner", "nail javascript"]
+var tasks = ["buy milk", "eat dinner", "nail javascript"];
+var toDoItem = document.createElement("li");
+
+for (let i = 0; i < tasks.length; i++) {
+    toDoItem.textContent = tasks[i];
+    
+}
+
